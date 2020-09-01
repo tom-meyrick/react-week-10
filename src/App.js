23749b8570e120be728d2cbbbb85a722259994e4
Tom Meyrick <tomgmeyrick@gmail.com>
2020-09-01 16:03:05 +0100

    Q1 lifting state up challenges completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Squares from "./components/Squares";
 import SignUp from "./components/SignUp";
+import Button from "./components/Button";
 
 const App = () => (
   <>
+    <h1>One-way Data Flow</h1>
+    <h3>Lifting State</h3>
     <Squares color={"hotpink"} />
     <SignUp minimumLength={4} />
+    <h3>Passing value up</h3>
+    <Button handleUpdate={(value) => console.log(value)} />
   </>
 );
 
---
+++ src/components/Button.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+// Create a component <Button> which keeps track of how many times it's been clicked (it doesn't need to display it anywhere). It should have a handleUpdate prop which accepts a function. Whenever the value of the <Button> changes it should also call the handleUpdate function and pass it the current value. Use the <Button> as follows and check that it logs the value to the console each time the button is clicked.
+
+{
+  /* <Button handleUpdate={ console.log } /> */
+}
+
+class Button extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  componentDidUpdate(prevState) {
+    let { handleUpdate } = this.props;
+    handleUpdate(this.state.counter);
+  }
+
+  render() {
+    let { counter } = this.state;
+    return (
+      <>
+        <p>{counter}</p>
+        <button className="btn btn-primary" onClick={this.handleClick}>
+          Click me
+        </button>
+      </>
+    );
+  }
+}
+
+export default Button;
